caeb7b0f7e5e9c07d0ea2d31ef548eb29d97fc29
Your Name <you@example.com>
2017-06-10 20:20:32 +0800

    added new post feature

--- blog/forms.py
+++ blog/forms.py
@@ -1,6 +1,16 @@
 from django import forms
+from .models import Post
 from django.contrib.auth.models import User
 
+class PostForm(forms.ModelForm):
+    
+    class Meta:
+        model = Post
+        fields = ('title', 'text',)
+    '''
+    title = forms.CharField(label="Title:", max_length=100)
+    text = forms.CharField(label="Content:")
+'''
 class AuthorForm(forms.Form):
     first_name = forms.CharField(label="First Name", max_length=100)
     last_name = forms.CharField(label="Last Name", max_length=100)
