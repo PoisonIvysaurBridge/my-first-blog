b8bde66156031a310783f0e2fece395b1adb99ab
Your Name <you@example.com>
2017-06-10 18:13:29 +0800

    post details added

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.utils import timezone
 from .forms import AuthorForm
 from .models import Post #, Author
@@ -6,6 +6,10 @@ from .models import Post #, Author
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
 '''
 def index(request):
     
